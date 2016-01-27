f9aa32b4a41a8151661422e738c151f10ed971ba
lordfluffypants <rixmaddi@gmail.com>
2016-01-27 15:14:49 -0500

    added final python client and added local host server class

--- Assignment 2 - Simple TCP Client/John_Wesley_Hayhurst_Client.py
+++ Assignment 2 - Simple TCP Client/John_Wesley_Hayhurst_Client.py
@@ -1,15 +1,11 @@
 import socket
 import sys
-
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(enigma.pcs.cnu.edu,3223)
+socket.connect(('enigma.pcs.cnu.edu',3223))
 try:
-    number_rec = socket.recv()
-    print number_rec
-    number_send = int(number_rec) + 1
-    socket.sendall(number_send)
-    print socket.recv()
-    if socket.recv() == "SEND_NAME":
-        socket.sendall("John Wesley McDonald Hayhurst")
+    number_rec = socket.recv(16)
+    number_send = str(int(number_rec) + 1)+"\n"
+    socket.send(number_send)
+    socket.send("John Wesley McDonald Hayhurst\n")
 finally:
     socket.close()
