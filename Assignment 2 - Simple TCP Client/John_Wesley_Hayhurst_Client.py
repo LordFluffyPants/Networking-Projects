7e645e2f30d744526aa9dcbabfa65746c4f659dd
lordfluffypants <rixmaddi@gmail.com>
2016-03-09 10:11:19 -0500

    updated client

--- Assignment 2 - Simple TCP Client/John_Wesley_Hayhurst_Client.py
+++ Assignment 2 - Simple TCP Client/John_Wesley_Hayhurst_Client.py
@@ -1,11 +1,14 @@
 import socket
+import time
 import sys
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(('enigma.pcs.cnu.edu',3223))
+socket.connect(('127.0.0.1',4444))
 try:
     number_rec = socket.recv(16)
     number_send = str(int(number_rec) + 1)+"\n"
     socket.send(number_send)
+    dummy = socket.recv(16)
     socket.send("John Wesley McDonald Hayhurst\n")
 finally:
+    time.sleep(10)
     socket.close()
